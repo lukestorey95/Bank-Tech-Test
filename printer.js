7652c6976418aae56be50ef115e6852267e05856
Luke Storey <lukemstorey@gmail.com>
2022-06-21 16:05:24 +0100

    Refacotor to use separate map callback

--- printer.js
+++ printer.js
@@ -6,16 +6,16 @@ class Printer {
   }
 
   #formatTransactions(transactions) {
-    return transactions
-      .map((transaction) => {
-        return [
-          transaction.getDate(),
-          transaction.getCredit(),
-          transaction.getDebit(),
-          transaction.getBalance(),
-        ].join(" || ");
-      })
-      .join("\n");
+    return transactions.map(this.#formatTransaction).join("\n");
+  }
+
+  #formatTransaction(transaction) {
+    return [
+      transaction.getDate(),
+      transaction.getCredit(),
+      transaction.getDebit(),
+      transaction.getBalance(),
+    ].join(" || ");
   }
 }
 
